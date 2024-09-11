c8f6230f7e7d066e524d356fbc46da86f2c08b86
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-09-11 19:22:43 -0300

    feat: global constants

---
+++ src/constants/index.ts
@@ -0,0 +1,35 @@
+import { ActionColors } from "@/interfaces/Chart/types";
+
+export const HAND_RANGES: string[][] = [
+    ["AA", "AKs", "AQs", "AJs", "ATs", "A9s", "A8s", "A7s", "A6s", "A5s", "A4s", "A3s", "A2s"],
+    ["AKo", "KK", "KQs", "KJs", "KTs", "K9s", "K8s", "K7s", "K6s", "K5s", "K4s", "K3s", "K2s"],
+    ["AQo", "KQo", "QQ", "QJs", "QTs", "Q9s", "Q8s", "Q7s", "Q6s", "Q5s", "Q4s", "Q3s", "Q2s"],
+    ["AJo", "KJo", "QJo", "JJ", "JTs", "J9s", "J8s", "J7s", "J6s", "J5s", "J4s", "J3s", "J2s"],
+    ["ATo", "KTo", "QTo", "JTo", "TT", "T9s", "T8s", "T7s", "T6s", "T5s", "T4s", "T3s", "T2s"],
+    ["A9o", "K9o", "Q9o", "J9o", "T9o", "99", "98s", "97s", "96s", "95s", "94s", "93s", "92s"],
+    ["A8o", "K8o", "Q8o", "J8o", "T8o", "98o", "88", "87s", "86s", "85s", "84s", "83s", "82s"],
+    ["A7o", "K7o", "Q7o", "J7o", "T7o", "97o", "87o", "77", "76s", "75s", "74s", "73s", "72s"],
+    ["A6o", "K6o", "Q6o", "J6o", "T6o", "96o", "86o", "76o", "66", "65s", "64s", "63s", "62s"],
+    ["A5o", "K5o", "Q5o", "J5o", "T5o", "95o", "85o", "75o", "65o", "55", "54s", "53s", "52s"],
+    ["A4o", "K4o", "Q4o", "J4o", "T4o", "94o", "84o", "74o", "64o", "54o", "44", "43s", "42s"],
+    ["A3o", "K3o", "Q3o", "J3o", "T3o", "93o", "83o", "73o", "63o", "53o", "43o", "33", "32s"],
+    ["A2o", "K2o", "Q2o", "J2o", "T2o", "92o", "82o", "72o", "62o", "52o", "42o", "32o", "22"],
+];
+
+export const COLORS_FIRST_TO_ACT: ActionColors = {
+    fold: "#FFFFFF", 
+    open_fold_vs_3bet: "#F28B82",
+    open_call_vs_3bet: "#AECBFA",
+    open_4bet_fold_vs_3bet: "#81C995",
+    open_4bet_call_vs_3bet: "#FBBC04",
+  };
+  
+export const COLORS_SECOND_TO_ACT: ActionColors = {
+    fold: "#FFFFFF", 
+    call_vs_open: "#AECB",
+    call_vs_mini_raise: "#00008B",
+    "3bet_fold_vs_4bet": "#C33C54",
+    "3bet_call_vs_4bet": "#FFEB3B",
+    "3bet_all_in_vs_4bet": "#F4511E",
+};
+  
