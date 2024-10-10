8084e98180443065387ab4e8242dfeed120b44eb
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-10-10 18:32:54 -0300

    refactor: use of context and calls googleSignIn

--- src/components/HomeForm/index.tsx
+++ src/components/HomeForm/index.tsx
@@ -1,26 +1,24 @@
-// create a form to handle sign in and sign up
+"use client";
 
-'use client';
-
-import { useState } from "react";
-import SignInButton from "../SignInButton";
+import { UserContext } from "@/context/UserContext";
+import { handleGoogleSignIn } from "@/services/auth/googleAuth";
+import { useContext } from "react";
 
 const HomeForm = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
+  const { setUser } = useContext(UserContext);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setLoading(true);
-    setLoading(false);
-  };
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const user = await handleGoogleSignIn();
 
+    if (user)
+      setUser(user);
+  }
+ 
   return (
     <section className="bg-gray-700 p-4 rounded-lg p-10 h-96 w-96 mt-10 flex flex-col items-center justify-center">
         <form onSubmit={handleSubmit}>
-            <SignInButton />
+          <button type="submit" className="bg-gradient-to-r from-red-500 via-yellow-500 via-green-500 to-blue-500 bg-clip-text text-transparent p-2 rounded-lg cursor-pointer">Sign in with Google</button>
         </form>
     </section>
   );
