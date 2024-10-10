c3141add01cc5f147a6b1631506d6f0fd1854394
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-10-10 18:30:27 -0300

    fix: delete components

--- src/components/Header/index.tsx
+++
@@ -1,9 +0,0 @@
-import SignInButton from "../SignInButton";
-
-export default function Header() {
-    return (
-        <header className="flex justify-end items-center">
-            <SignInButton />
-        </header>
-    )
-}
