62a2ac941e87509d27d0e1f0ec19a8355e2c9ecd
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-10-10 18:35:08 -0300

    refactor: adds data to session storage and updates user context

--- src/services/auth/googleAuth.ts
+++ src/services/auth/googleAuth.ts
@@ -1,8 +1,10 @@
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { auth } from '@/firebaseConfig';
 import { saveUserData } from '@/services/user/userService';
+import { redirectHomeIfLoggedIn } from '../actions';
+import { ProvidedUserData } from '@/context/UserContext/types';
 
-export const handleGoogleSignIn = async () => {
+export const handleGoogleSignIn = async (): Promise<ProvidedUserData | undefined> => {
   const provider = new GoogleAuthProvider();
   try {
     const result = await signInWithPopup(auth, provider);
@@ -10,7 +12,11 @@ export const handleGoogleSignIn = async () => {
     if (user) {
       await saveUserData(user);
     }
+    sessionStorage.setItem('user', JSON.stringify(user));
+    redirectHomeIfLoggedIn();
+
+    return user.providerData[0]; 
   } catch (error) {
     console.error('Error signing in with Google:', error);
-  }
+  } 
 };
