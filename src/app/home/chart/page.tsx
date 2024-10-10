aa1b8cb56ff15b6cfe665c128d3cc2d5715885ae
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-10-10 18:34:24 -0300

    feat: home route page

---
+++ src/app/home/chart/page.tsx
@@ -0,0 +1,5 @@
+import { Chart } from "@/components/Chart";
+
+export default function ChartPage() {
+  return <Chart isFirstToAct={true} />;
+}
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,9 @@
-import { Chart } from "@/components/Chart";
+import Aside from "@/components/Aside";
 
-export default function Home() {
+export default async function Home() {
   return (
-    <main className="min-h-screen flex-col items-center justify-between p-24">
-      <Chart isFirstToAct={true} />
-    </main>
+      <main className="min-h-screen flex-col items-center justify-between p-24">
+        <Aside />
+      </main>
   );
 }
