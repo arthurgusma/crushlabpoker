f030a22c69a427bf76cf89ede9bcb1dc38d42a06
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-10-18 10:57:01 -0300

    fix: layout

--- src/components/HomeForm/index.tsx
+++ src/components/HomeForm/index.tsx
@@ -16,9 +16,9 @@ const HomeForm = () => {
   }
  
   return (
-    <section className="bg-gray-700 p-4 rounded-lg p-10 h-96 w-96 mt-10 flex flex-col items-center justify-center">
+    <section className="flex flex-col items-center justify-center">
         <form onSubmit={handleSubmit}>
-          <button type="submit" className="bg-gradient-to-r from-red-500 via-yellow-500 via-green-500 to-blue-500 bg-clip-text text-transparent p-2 rounded-lg cursor-pointer">Sign in with Google</button>
+          <button type="submit" className="text-main-light-green p-2 rounded-lg cursor-pointer bg-main-gold opacity-75 hover:opacity-100">Sign in with Google</button>
         </form>
     </section>
   );
