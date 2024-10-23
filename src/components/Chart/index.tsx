3390a5d72d771cc7690b6fc51334111712d7b310
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-10-23 20:43:59 -0300

    feat: function to handle user color

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -3,14 +3,17 @@ import React from "react";
 import { HAND_RANGES } from "@/constants";
 import { ChartProps } from "./types";
 
-export const Chart: React.FC<ChartProps> = ({ range, isRFI }) => {
+export const Chart: React.FC<ChartProps> = ({ range }) => {
 
-  const getHandColor = (hand: string) => {
-    if (isRFI && range.raiseFirstIn.includes(hand)) return "bg-chart-green";
-    if (range.foldTo3Bet.includes(hand)) return "bg-chart-red text-main-champagne";
-    if (range.call3Bet.includes(hand)) return "bg-chart-green";
-    if (range.fourBetAndFold.includes(hand)) return "bg-chart-blue text-main-champagne";
-    if (range.fourBetAndCall.includes(hand)) return "bg-chart-yellow";
+  const handleGetHandColor = (hand: string) => {
+    if (range.raiseFirstIn?.includes(hand)) return "bg-chart-green";
+    if (range.foldTo3Bet?.includes(hand)) return "bg-chart-red text-main-champagne";
+    if (range.call3Bet?.includes(hand)) return "bg-chart-green";
+    if (range.fourBetAndFold?.includes(hand)) return "bg-chart-blue text-main-champagne";
+    if (range.fourBetAndCall?.includes(hand)) return "bg-chart-yellow";
+    if (range.threeBet?.includes(hand)) return "bg-chart-orange";
+    if (range.shoveVs4Bet?.includes(hand)) return "bg-chart-purple";
+    if (range.call?.includes(hand)) return "bg-chart-pink";
     return "bg-main-champagne"; 
   };
 
@@ -22,7 +25,7 @@ export const Chart: React.FC<ChartProps> = ({ range, isRFI }) => {
             {row.map((hand) => (
               <td
                 key={hand}
-                className={`w-12 h-12 border border-black p-2 text-center text-black ${getHandColor(
+                className={`w-12 h-12 border border-black p-2 text-center text-black ${handleGetHandColor(
                   hand
                 )}`}
               >
--- src/components/Chart/types.ts
+++ src/components/Chart/types.ts
@@ -1,12 +1,14 @@
-interface Range {
-    raiseFirstIn: string[];
-    foldTo3Bet: string[];
-    call3Bet: string[];
-    fourBetAndFold: string[];
-    fourBetAndCall: string[];
+export interface Range {
+    raiseFirstIn?: string[];
+    foldTo3Bet?: string[];
+    call3Bet?: string[];
+    fourBetAndFold?: string[];
+    fourBetAndCall?: string[];
+    threeBet?: string[];
+    shoveVs4Bet?: string[];
+    call?: string[];
   }
   
 export interface ChartProps {
     range: Range;
-    isRFI: boolean;
 }
