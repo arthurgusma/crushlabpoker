03e66ad909e9299ac0cbc03afcb2b1833b1fdbdd
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-10-23 20:38:59 -0300

    feat: control of range passed to chart

--- src/components/ChartOptions/index.tsx
+++ src/components/ChartOptions/index.tsx
@@ -1,30 +1,36 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Chart } from "../Chart";
 import { TABLE_POSITIONS } from "@/constants";
-import { handleSelection } from "./helpers";
+import { handleSelection, handleSelectRange } from "./helpers";
 import ChartOptionsButton from "./ChartOptionsButton";
+import { Range } from "../Chart/types";
 
 export default function ChartOptions() {
     const [selectedPosition, setSelectedPosition] = useState<string>("UTG");
     const [selectedAction, setSelectedAction] = useState<string>("RFI");
+    const [range, setRange] = useState<Range>({} as Range);
+    
+    useEffect(()=>{
+        setRange(handleSelectRange(selectedPosition, selectedAction));
+    },[selectedPosition, selectedAction])
 
     return (
         <div>
             <h1 className="mb-2 text-main-gold">Selecione sua posição:</h1>
             <section className="flex justify-between mb-4">
             {TABLE_POSITIONS.map((position) => (
-               <ChartOptionsButton position={position} selectedPosition={selectedPosition} setSelectedPosition={setSelectedPosition} />
+               <ChartOptionsButton  key={position} position={position} selectedPosition={selectedPosition} setSelectedPosition={setSelectedPosition} />
             ))}
             </section>
             <h1 className="mb-2 text-main-gold">Selecione a ação:</h1>
             <section className="flex justify-between mb-4">
                 {handleSelection(selectedPosition).map((action) => (
-                    <ChartOptionsButton position={action} selectedPosition={selectedAction} setSelectedPosition={setSelectedAction} />
+                    <ChartOptionsButton key={action} position={action} selectedPosition={selectedAction} setSelectedPosition={setSelectedAction} />
                 ))}
             </section >
-            <Chart />
+            <Chart range={range} />
         </div>
     )
 }
