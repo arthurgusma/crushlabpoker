c855ee206b07a87daf8eec82e8271013fe2db9c5
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-10-29 12:05:20 -0300

    feat: bb ranges

---
+++ src/constants/bbRanges.ts
@@ -0,0 +1,29 @@
+export const BB_VS_UTG_RFI = {
+    call: ["A9s", "A8s", "A7s", "A6s", "A4s", "A3s", "A2s", "K9s", "K8s", "K7s", "K6s", "K5s", "K4s", "K3s", "K2s", "AQo", "KQo", "Q9s", "Q8s", "Q7s", "Q6s", "Q5s", "AJo", "KJo", "QJo", "J9s", "J8s", "J7s", "ATo", "KTo", "QTo", "JTo", "T9s", "T8s", "T7s", "98s", "97s", "96s", "88", "87s", "86s", "85s", "77", "75s", "74s", "66", "64s", "63s", "55", "53s", "52s", "44", "43s", "42s", "33", "32s", "22"],
+    threeBet: ["AKs", "AQs", "AJs", "ATs", "A5s", "A4s", "AKo", "KQs", "KJs", "KTs", "AQo", "QQ", "QJs", "QTs", "JJ", "JTs", "TT", "99", "76s", "65s", "54s"],
+    shoveVs4Bet: ["AA", "KK"],
+}
+
+export const BB_VS_HJ_RFI = {
+    call: ["A9s", "A8s", "A7s", "A6s", "A5s", "A4s", "A3s", "A2s", "K9s", "K8s", "K7s", "K6s", "K5s", "K4s", "K3s", "K2s", "AQo", "A9o", "KQo", "Q9s", "Q8s", "Q7s", "Q6s", "Q5s", "Q4s", "J6s", "T6s", "Q3s", "AJo", "KJo", "QJo", "J9s", "J8s", "J7s", "ATo", "KTo", "QTo", "JTo", "T9s", "T8s", "T7s", "98s", "97s", "96s", "88", "87s", "86s", "85s", "77", "75s", "74s", "66", "64s", "63s", "55", "53s", "52s", "44", "43s", "42s", "33", "32s", "22"],
+    threeBet: ["QQ", "AKo", "AKs", "AQs", "AJs", "ATs",  "A5s", "KQs", "KJs", "KTs", "AQo","QJs", "QTs", "JJ", "JTs", "TT", "T9s", "99", "98s", "88", "87s", "76s", "65s", "54s"],
+    shoveVs4Bet: ["AA", "KK",],
+}
+
+export const BB_VS_CO_RFI = {
+    call: ["A8s", "A7s", "A6s", "A5s", "A4s", "A3s", "A2s", "K9s", "K8s", "K7s", "K6s", "K5s", "K4s", "K3s", "K2s", "AQo", "A9o", "KQo", "Q9s", "Q8s", "Q7s", "Q6s", "Q5s", "Q4s", "Q3s", "Q2s", "J6s","J5s","J4s", "T6s", "T5s","AJo", "A8o", "A5o", "KJo", "K9o", "T9o","98o", "QJo", "J9s", "J8s", "J7s", "ATo", "KTo", "QTo", "JTo", "T9s", "T8s", "T7s", "98s", "97s", "96s", "95s", "84s", "88", "87s", "86s", "85s", "77", "75s", "74s", "66", "64s", "63s", "55", "53s", "52s", "44", "43s", "42s", "33", "32s", "22"],
+    threeBet: ["AQs", "AJs", "ATs", "A9s", "A5s", "KQs", "KJs", "KTs", "K9s", "AQo", "KQo", "QJs", "QTs", "Q9s", "AJo", "JJ", "JTs", "J9s", "TT", "T9s", "T8s", "99", "98s", "88", "87s", "77", "76s", "65s", "54s"],
+    shoveVs4Bet: ["AA", "KK", "QQ", "AKo", "AKs"],
+}
+
+export const BB_VS_BTN_RFI = {
+    call: ["A7s", "A4s", "A3s", "A2s", "K9s", "K8s", "K7s", "K6s", "K5s", "K4s", "K3s", "K2s", "A9o", "Q7s", "Q6s", "Q5s", "J3s", "T4s", "Q4s", "Q3s", "Q2s", "J6s", "J5s", "J4s", "T3", "T6s", "T5s", "AJo", "A8o", "A5o", "KJo", "K9o", "T9o", "98o", "QJo", "J9s", "J8s", "J7s", "J2s", "T3s","ATo", "KTo", "QTo", "JTo", "T9s", "T8s", "T7s", "98s", "97s", "96s", "95s", "84s", "88", "87s", "86s", "85s", "77", "75s", "74s", "66", "64s", "63s", "55", "53s", "52s", "44", "43s", "42s", "33", "32s", "22", "87o", "76o", "65o", "54o", "A7o", "A6o", "A4o", "A3o", "Q9o", "J9o", "K8o", "J8o", "T8o", "97o"],
+    threeBet: ["AQs", "AJs", "ATs", "A9s", "A6s", "A5s", "KQs", "KJs", "KTs", "K9s", "AQo", "KQo", "QJs", "QTs", "Q9s", "AJo", 'KJo', "QJo", "ATo", "KTo", "QTo", "JJ", "JTs", "J9s", "TT", "T9s", "T8s", "99", "98s", "88", "87s", "77", "76s", "65s", "54s", "A8s", "K8s", "Q8s", "J8s", "T8s", "98s", "87s", "76s", "65s", "54s", "97s", "86s", "75s", "64s", "53s", "66", "55"],
+    shoveVs4Bet: ["AA", "KK", "AKo", "AKs", "QQ"],
+}
+
+export const BB_VS_SB_RFI = {
+    call: ["A8s", "A7s", "A6s", "A4s", "A3s", "A2s", "K9s", "K8s", "K7s", "K6s", "K5s", "K4s", "K3s", "K2s", "AQo", "A9o", "KQo", "Q9s", "Q8s", "Q7s", "Q6s", "Q5s", "Q4s", "Q3s", "Q2s", "J6s","J5s","J4s", "T6s", "T5s","AJo", "A8o", "A5o", "KJo", "K9o", "T9o","98o", "QJo", "J9s", "J8s", "J7s", "ATo", "KTo", "QTo", "JTo", "T9s", "T8s", "T7s", "98s", "97s", "96s", "95s", "84s", "88", "87s", "86s", "85s", "77", "75s", "74s", "66", "64s", "63s", "55", "53s", "52s", "44", "43s", "42s", "33", "32s", "22", "J3s", "T3s", "J2s", "T2s", "T4s", "94s", "93s", "92s", "83s", "82s", "73s", "72s", "62s", "A7o", "A6o", "A4o", "A3o", "Q9o", "J9o", "K8o", "J8o", "T8o", "97o", "K7o", "Q7o", "J7o", "T7o", "86o", "76o", "65o", "54o", "Q8o", "87o", "76s", "54s", "A2o", "K6o", "K5o", "K4o", "K3o", "K2o", "Q6o", "85o", "75o", "53o", "64o"],
+    threeBet: ["AQs", "AJs", "ATs", "A9s", "A5s",  "KQs", "KJs", "KTs", "AQo", "QJs", "QTs", "JJ", "JTs", "TT", "99", "88", "K9s", "Q9s", "J9s", "T9s", "98s", "65s", "KQo", "AJo", "ATo"],
+    shoveVs4Bet: ["AA", "KK", "AKs", "AKo", "QQ"],
+}
