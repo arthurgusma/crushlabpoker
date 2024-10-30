12677a592eacffe19d20fe7bc5aeb1a2996ceb07
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-10-30 15:42:06 -0300

    fix: removes unused styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import HomeForm from "@/components/HomeForm";
 
 export default async function LogIn() {
   return (
-    <main className="min-h-screen flex-col items-center justify-between p-24">
-      <section className="flex items-center justify-between mx-10">
-        <div className="flex flex-col items-center justify-center  w-6/12">
+    <main className="flex flex-col items-center justify-between">
+      <section className="flex items-center justify-between w-full min-h-screen">
+        <div className="flex flex-col items-center justify-center w-6/12 h-screen">
           <h1 className="text-4xl font-bold">Crush Lab Poker</h1>
           <p className="text-lg">Level up your poker skills</p>
         </div>
-        <div className="w-6/12 flex items-center justify-center">
+        <div className="w-6/12 flex items-center justify-center h-screen bg-main-champagne">
           <HomeForm />
         </div>
       </section>
