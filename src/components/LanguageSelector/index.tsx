f8e42b0adc078b169e94f65fcceb118e6c2e4ef0
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-10-30 15:34:16 -0300

    feat: languague selection

---
+++ src/components/LanguageSelector/index.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import i18n from '@/i18n';
+import BrasilFlag from "@/assets/brazil-flag.png";
+import EuaFlag from "@/assets/usa-flag.png";
+import Image from "next/image";
+
+export default function LanguageSelector() {
+    const [selectedLanguage, setSelectedLanguage] = useState("");
+
+    function chooseLanguage(language: string) {
+        i18n.changeLanguage(language);   
+        setSelectedLanguage(language);
+        localStorage.setItem('language', language);
+    }
+
+    useEffect(() => {
+        const storedLanguage = localStorage.getItem('language') || "en-US";
+        i18n.changeLanguage(storedLanguage);
+        setSelectedLanguage(storedLanguage);
+    }, []);
+
+    return (
+        <div className="flex flags-container">
+           <Image alt="flag" src={BrasilFlag} className={`mx-1 ${selectedLanguage !== "pt-BR" && "grayscale"} hover:cursor-pointer hover:grayscale-0`}  height={30} onClick={() => chooseLanguage("pt-BR")} />
+           <Image alt="flag" src={EuaFlag} className={`mx-1 ${selectedLanguage !== "en-US" && "grayscale"} hover:cursor-pointer hover:grayscale-0`}  height={30} onClick={() => chooseLanguage("en-US")} />
+        </div>
+      )
+};
