6b707290b0e4a2b6fbc6bc76d9b43048edb96f95
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-10-30 11:57:20 -0300

    fix: turn off debug mode

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -15,16 +15,15 @@ const resources = {
 };
 
 i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(initReactI18next)
   .init({
-    resources, // resources are important to load translations for the languages.
-    lng: "en", // It acts as default language. When the site loads, content is shown in this language.  
-    debug: true,
-    fallbackLng: "pt", // use de if selected language is not available
+    resources,
+    lng: "en",
+    fallbackLng: "pt",
     interpolation: {
       escapeValue: false
     },
-    ns: "translation", // namespaces help to divide huge translations into multiple small files.
+    ns: "translation",
     defaultNS: "translation"
   });
 
