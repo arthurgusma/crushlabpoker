5e8e793f714e2b16e8b14f7a36014e3a1fc15cbc
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-10-31 11:40:34 -0300

    feat: translation to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,19 @@
+"use client";
+
 import LanguageSelector from "@/components/LanguageSelector";
+import { useTranslation } from "react-i18next";
 
 export default function Header() {
+    const { t } = useTranslation();
+
     return (
         <header className="flex justify-between items-center p-4 bg-main-green">
             <h1 className="text-2xl text-main-gold">Crush Lab Poker</h1>
             <nav>
                 <ul className="flex">
-                    <li className="mx-2"><a href="#" className="text-main-champagne">Home</a></li>
-                    <li className="mx-2"><a href="#" className="text-main-champagne">About</a></li>
-                    <li className="mx-2"><a href="#" className="text-main-champagne">Profile</a></li>
+                    <li className="mx-2"><a href="#" className="text-main-champagne">{t("header.home")}</a></li>
+                    <li className="mx-2"><a href="#" className="text-main-champagne">{t("header.about")}</a></li>
+                    <li className="mx-2"><a href="#" className="text-main-champagne">{t("header.profile")}</a></li>
                     <li className="mx-2"><LanguageSelector /></li>
                 </ul>
             </nav>
