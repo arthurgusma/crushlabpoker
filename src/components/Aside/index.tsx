fce05bd35cd079b74d360e0af6669ec48f0926b0
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-11-07 14:22:25 -0300

    feat: translation to aside menu

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -3,10 +3,12 @@
 import { UserContext } from "@/context/UserContext";
 import Image from "next/image"
 import { useContext } from "react";
+import { useTranslation } from "react-i18next";
 
 export default function Aside() {
     const { user } = useContext(UserContext);
 
+    const { t } = useTranslation();
     const photoSrc = user.photoURL || "";
 
     return (
@@ -14,9 +16,10 @@ export default function Aside() {
             <Image src={photoSrc} alt="Profile Image" width={60} height={60} className="rounded-full"/>
             <p></p>
             <ul>
-                <li>Assinatura</li>
-                <li>Sobre</li>
-                <li>Ajuda</li>
+                <li>{t("aside.subscription")}</li>
+                <li>{t("aside.about")}</li>
+                <li>{t("aside.help")}</li>
+                <li>{t("aside.logout")}</li>
             </ul>
         </aside>
     )
