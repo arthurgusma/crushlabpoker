91566c6ad398444edeb2b604d778bd2e8e5662f5
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-11-07 14:04:14 -0300

    fix: removes scroll from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ import HomeForm from "@/components/HomeForm";
 export default async function LogIn() {
   return (
     <main className="flex flex-col items-center justify-between">
-      <section className="flex items-center justify-between w-full">
-        <div className="flex flex-col items-center justify-center w-6/12 h-screen">
+      <section className="flex items-center justify-between w-full" style={{ height: 'calc(100vh - 64px)' }}>
+        <div className="flex flex-col items-center justify-center w-6/12 h-full ">
           <h1 className="text-4xl font-bold">Crush Lab Poker</h1>
           <p className="text-lg">Level up your poker skills</p>
         </div>
-        <div className="w-6/12 flex items-center justify-center h-screen bg-main-champagne">
+        <div className="w-6/12 flex items-center justify-center bg-main-champagne h-full">
           <HomeForm />
         </div>
       </section>
