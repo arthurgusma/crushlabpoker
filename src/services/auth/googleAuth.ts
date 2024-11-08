19dd1df98fbe5ca1c6d870f68464dd3eec2c47a0
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-11-08 17:02:12 -0300

    refactor: use of cookies and middleware

--- src/services/auth/googleAuth.ts
+++ src/services/auth/googleAuth.ts
@@ -1,21 +1,24 @@
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { auth } from '@/firebaseConfig';
 import { saveUserData } from '@/services/user/userService';
-import { redirectHomeIfLoggedIn } from '../actions';
 import { ProvidedUserData } from '@/context/UserContext/types';
+import { redirectHomeIfLoggedIn, setCookieSession } from '@/services/actions';
 
 export const handleGoogleSignIn = async (): Promise<ProvidedUserData | undefined> => {
   const provider = new GoogleAuthProvider();
   try {
     const result = await signInWithPopup(auth, provider);
     const user = result?.user;
-    if (user) {
-      await saveUserData(user);
-    }
-    sessionStorage.setItem('user', JSON.stringify(user));
-    redirectHomeIfLoggedIn();
 
-    return user.providerData[0]; 
+    if (user) {
+      const { uid } = user;
+      const expiresAt = new Date(Date.now() + 72 * 60 * 60 * 1000).toISOString();
+      await saveUserData(user);
+      setCookieSession(uid, expiresAt);
+
+     redirectHomeIfLoggedIn();
+     return user.providerData[0]; 
+    }
   } catch (error) {
     console.error('Error signing in with Google:', error);
   } 
