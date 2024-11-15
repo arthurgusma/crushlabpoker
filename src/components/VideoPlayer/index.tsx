f9b94c0b0880e073d1b11ce044bf932a2aafa7e4
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-11-15 16:48:38 -0300

    fix: video plays from iframe

--- src/components/VideoPlayer/index.tsx
+++ src/components/VideoPlayer/index.tsx
@@ -1,9 +1,8 @@
-"use client";
-
 export default function VideoPlayer() {
     return (
-      <video width="320" height="240" controls preload="none">
-        <source src="/src/public/assets/demo/demoCrushLabEN.mp4" type="video/mp4" />
-      </video>
-    )
-  }
+      <iframe
+        style={{ height: "500px", width: "1000px" }}
+        src="https://player.vimeo.com/video/1030141552?badge=0&autopause=0&player_id=0&app_id=58479"
+      />
+  );
+}
