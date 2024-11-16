08ba5a880e91709a93f84326e23ec94a57b3b2a8
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-11-16 12:11:03 -0300

    feat: a border to selected lang

--- src/components/LanguageSelector/index.tsx
+++ src/components/LanguageSelector/index.tsx
@@ -23,8 +23,16 @@ export default function LanguageSelector() {
 
     return (
         <div className="flex flags-container">
-           <Image alt="brazilian flag" src={BrasilFlag} className={`mx-1 cursor-pointer ${selectedLanguage !== "pt-BR" ? "grayscale" : "cursor-not-allowed"} hover:grayscale-0`}  height={30} onClick={() => chooseLanguage("pt-BR")} />
-           <Image alt="usa flag" src={EuaFlag} className={`mx-1 cursor-pointer ${selectedLanguage !== "en-US" ? "grayscale" : "cursor-not-allowed"}  hover:grayscale-0`}  height={30} onClick={() => chooseLanguage("en-US")} />
-        </div>
+            <Image alt="brazilian flag" src={BrasilFlag} height={30} onClick={() => chooseLanguage("pt-BR")}
+                className={`mx-1 cursor-pointer border-2 rounded-full ${
+                    selectedLanguage === "pt-BR" ? "border-green-500 cursor-not-allowed" : "grayscale border-transparent"
+                }`}
+            />
+            <Image alt="usa flag" src={EuaFlag} height={30} onClick={() => chooseLanguage("en-US")}
+                className={`mx-1 cursor-pointer border-2 rounded-full ${
+                    selectedLanguage === "en-US" ? "border-green-500 cursor-not-allowed" : "grayscale border-transparent"
+                }`}
+            />
+      </div>
       )
 };
