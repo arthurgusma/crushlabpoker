792caf9ba7009e031e0f6c1d3ce32629dbb07326
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-11-29 15:32:36 -0300

    fix: type of request

--- src/app/api/checkout_sessions/route.ts
+++ src/app/api/checkout_sessions/route.ts
@@ -1,8 +1,8 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
-export async function POST(request: NextRequest) {
+export async function POST(request: Request) {
   const { headers } = request;
   const origin = headers.get("origin");
 
