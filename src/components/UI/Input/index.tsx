ba7a75d220317b3ef8864ddc43d474fbdb5d510d
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-11-30 11:05:23 -0300

    feat: adjust layout for input

--- src/components/UI/Input/index.tsx
+++ src/components/UI/Input/index.tsx
@@ -2,14 +2,24 @@ import React, { forwardRef } from 'react';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   error?: string;
+  label: string;
 }
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ error, ...rest }, ref) => {
     return (
-      <div>
-        <label htmlFor={rest.name}>{rest.placeholder}</label>
-        <input ref={ref} {...rest} />
+      <div className='grid pb-2 relative w-full'>
+        <label 
+          htmlFor={rest.name} 
+          className="absolute left-3 top-1 text-main-brown text-sm"
+        >
+          {rest.label}
+        </label>
+        <input 
+          ref={ref} 
+          {...rest} 
+          className="peer w-full border-2 bg-gray-50 text-gray-900 focus:ring-1 focus:ring-main-brown px-3 pt-4 pb-2 rounded-md" 
+        />
         {error && <span>{error}</span>}
       </div>
     );
