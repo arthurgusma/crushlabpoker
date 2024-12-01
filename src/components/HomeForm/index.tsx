bd7f91ccb1efe76784fcf53fd8d2def117cd5b4a
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-12-01 15:01:58 -0300

    fix: translation and usage of react-icons

--- src/components/HomeForm/index.tsx
+++ src/components/HomeForm/index.tsx
@@ -1,8 +1,15 @@
 "use client";
 
 import { handleGoogleSignIn } from "@/services/auth/googleAuth";
+import { ButtonSubmit } from "../UI/Buttons";
+import { useTranslation } from "react-i18next";
+import { FcGoogle } from "react-icons/fc";
+import { useEffect } from "react";
+import i18n from "@/i18n";
 
 const HomeForm = () => {
+  const { t } = useTranslation();
+
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const user = await handleGoogleSignIn();
@@ -11,12 +18,19 @@ const HomeForm = () => {
       localStorage.setItem('user', JSON.stringify(user));
     }
   }
+
+  useEffect(() => {
+    const storedLanguage = localStorage.getItem('language');
+    if (storedLanguage) {
+        i18n.changeLanguage(storedLanguage);
+    }
+}, []);
  
   return (
     <section className="flex flex-col items-center justify-center">
-        <form onSubmit={handleSubmit}>
-          <button type="submit" className="text-main-light-green p-2 rounded-lg cursor-pointer bg-main-gold opacity-75 hover:opacity-100">Sign in with Google</button>
-        </form>
+      <form onSubmit={handleSubmit} className="pt-4">
+        <ButtonSubmit ><span className="flex content-center">{t("login-form.google")}<FcGoogle className="ml-1" size={25} /></span></ButtonSubmit>
+      </form>
     </section>
   );
 };
