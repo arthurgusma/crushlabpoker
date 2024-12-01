8ddcedccf42fca6d2849a4b23bcc48caba02e927
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-12-01 14:41:48 -0300

    feat: initial login signup form state

---
+++ src/components/AuthenticationForm/LogIn/index.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useEffect, useState } from 'react';
+import SignIn from '../SignUp';
+import { ButtonSubmit } from '@/components/UI/Buttons';
+import Input from '@/components/UI/Input';
+import { useTranslation } from 'react-i18next';
+import SwitchForm from '../SwitchForm';
+import i18n from '@/i18n';
+
+export default function LoginPage() {
+    const [isSignUp, setIsSignUp] = useState(false);
+
+    const { t } = useTranslation()
+    const schema = z.object({
+        email: z.string().email({ message: t("login-form.email-error") }),
+        password: z.string(),
+    })
+    
+    type LogInFormData = z.infer<typeof schema>
+    const { register, handleSubmit, formState: { errors } } = useForm<LogInFormData>({
+        resolver: zodResolver(schema),
+        reValidateMode: 'onChange'
+    })
+ 
+    async function onSubmit(data: LogInFormData) {
+        const response = await fetch('/api/auth/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data),
+        })
+
+        // TODO: Handle response
+
+        // if (response.ok) {
+        //     // router.push('/home')
+        // } else {
+        //     // Handle errors
+        // }
+    }
+
+    useEffect(()=>{
+        const storedLanguage = localStorage.getItem("languague");
+        if (storedLanguage) {
+            i18n.changeLanguage(storedLanguage);
+        }
+    },[])
+
+    return (
+        <>
+            {!isSignUp ? (
+                <>
+                    <form onSubmit={handleSubmit(onSubmit)} className='grid text-main-brown'>
+                        <Input 
+                            {...register("email")} 
+                            type="email"
+                            label={t("login-form.email")}
+                            error={errors.email?.message}
+                        />
+                        <Input 
+                            {...register("password")} 
+                            type="password" 
+                            label={t("login-form.password")}
+                            error={errors.password?.message} 
+                        />
+
+                        <div className='flex justify-center py-4'>
+                            <ButtonSubmit width='160'>{t("login-form.login")}</ButtonSubmit>
+                        </div>
+                    </form>
+                    <SwitchForm setisSignUp={setIsSignUp} isSignUp={isSignUp} buttonLabel={t("login-form.signup")} description={t("login-form.description-login")} />
+                </>
+
+            ) : (
+                <SignIn setisSignUp={setIsSignUp} isSignUp={isSignUp} />
+            )}
+        </>
+    )
+}
---
+++ src/components/AuthenticationForm/SignUp/index.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { ButtonSubmit } from "@/components/UI/Buttons";
+import { Dispatch, SetStateAction } from "react";
+import Input from "@/components/UI/Input";
+import { useTranslation } from "react-i18next";
+import SwitchForm from "../SwitchForm";
+
+interface SignInProps {
+    setisSignUp: Dispatch<SetStateAction<boolean>>;
+    isSignUp: boolean;
+}
+
+export default function SignIn({ setisSignUp, isSignUp }: SignInProps) {
+    const { t } = useTranslation();
+    const schema = z.object({
+        name: z.string({ message: t("login-form.name-error") }),
+        lastName: z.string({ message: t("login-form.last-name-error") }),
+        email: z.string().email({ message: t("login-form.email-error") }),
+        password: z.string().min(8, { message: t("login-form.password-error") }),
+        confirmPassword: z.string().min(8, { message:  t("login-form.password-error") })
+        }).refine(data => data.password === data.confirmPassword, {
+            message: t("login-form.confirm-password-error"),
+            path: ["confirmPassword"],
+        });
+    
+    type SigUpFormData = z.infer<typeof schema>;
+    const { register, handleSubmit, formState: { errors } } = useForm<SigUpFormData>({
+        resolver: zodResolver(schema),
+        reValidateMode: 'onChange',
+    });
+
+    async function onSubmit(data: SigUpFormData) {
+        const response = await fetch('/api/auth/signin', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data),
+        })
+
+        // TODO Handle response
+
+        // if (response.ok) {
+        //     // router.push('/home')
+        // } else {
+        //     // Handle errors
+        // }
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} className="grid text-main-brown">
+            <Input 
+                {...register("name")} 
+                type="string" 
+                label={t("login-form.name")}
+                error={errors.name?.message} 
+            />
+            <Input 
+                {...register("lastName")} 
+                type="string" 
+                label={t("login-form.last-name")}
+                error={errors.lastName?.message} 
+            />
+            <Input 
+                {...register("email")} 
+                type="email" 
+                label={t("login-form.email")}
+                error={errors.email?.message} 
+            />
+            <Input 
+                {...register("password")} 
+                type="password" 
+                label={t("login-form.password")}
+                error={errors.password?.message} 
+            />
+            <Input 
+                {...register("confirmPassword")} 
+                type="password" 
+                label={t("login-form.confirm-password")}
+                error={errors.confirmPassword?.message} 
+            />
+
+            <div className='flex justify-center py-4'>
+                <ButtonSubmit width='160'>{t("login-form.signup")}</ButtonSubmit>
+            </div>
+            <SwitchForm setisSignUp={setisSignUp} isSignUp={isSignUp} buttonLabel={t("login-form.login")} description={t("login-form.description-signup")}/>
+        </form>
+    );
+}
