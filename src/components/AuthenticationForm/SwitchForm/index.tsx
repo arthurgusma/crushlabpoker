7b1c726202f95ceba63224e5c66f8ab7ff2a64ce
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-12-13 17:56:40 -0300

    fix: typo

--- src/components/AuthenticationForm/SwitchForm/index.tsx
+++ src/components/AuthenticationForm/SwitchForm/index.tsx
@@ -2,14 +2,14 @@ import { ButtonText } from '@/components/UI/Buttons'
 import { Dispatch, SetStateAction } from 'react'
 
 interface SwitchFormProps {
-  setisSignUp: Dispatch<SetStateAction<boolean>>
+  setIsSignUp: Dispatch<SetStateAction<boolean>>
   isSignUp: boolean
   buttonLabel: string
   description: string
 }
 
 export default function SwitchForm({
-  setisSignUp,
+  setIsSignUp,
   isSignUp,
   buttonLabel,
   description,
@@ -17,7 +17,7 @@ export default function SwitchForm({
   return (
     <div className="flex justify-center text-main-light-green">
       <p className="mr-1 p-0">{description}</p>
-      <ButtonText onClick={() => setisSignUp(!isSignUp)}>
+      <ButtonText onClick={() => setIsSignUp(!isSignUp)}>
         {buttonLabel}
       </ButtonText>
     </div>
