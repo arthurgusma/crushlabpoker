3335088408c6bdc858ea7143a04bd3dd86e23d20
gusmadev <arthur.cabj@hotmail.com>
2025-01-08 16:58:58 -0300

    fix: responsivity on login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import HomeForm from '@/components/HomeForm'
 import Image from 'next/image'
 import logo from '@/assets/logo/full-logo.png'
+import logoPositivo from '@/assets/logo/logo-fundo-positivo.png'
 
 export default async function App() {
   return (
     <main className="flex flex-col items-center justify-between">
       <section
-        className="flex items-center justify-between w-full"
+        className="md:flex items-center justify-between w-full"
         style={{ height: 'calc(100vh - 64px)' }}
       >
-        <div className="flex flex-col items-center justify-center w-6/12 h-full bg-main-light-green">
+        <div className="hidden md:flex flex-col items-center justify-center w-6/12 h-full bg-main-light-green">
           <Image
             src={logo}
             alt="Crush Lab Poker Logo"
@@ -17,8 +18,15 @@ export default async function App() {
             height={800}
           />
         </div>
-        <div className="w-6/12 flex items-center justify-center bg-main-champagne h-full">
-          <div>
+        <div className="md:w-6/12 flex items-center justify-center bg-main-champagne h-full">
+          <div className="place-content-center text-center">
+            <Image
+              src={logoPositivo}
+              alt="Crush Lab Poker Logo"
+              width={200}
+              height={200}
+              className="md:hidden mb-14 mx-auto"
+            />
             <HomeForm />
           </div>
         </div>
