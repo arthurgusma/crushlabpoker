eea5afc25c9aba16b917e734caaef31ddd74f775
gusmadev <arthur.cabj@hotmail.com>
2025-01-09 16:24:45 -0300

    feat: TextArea

--- src/components/UI/Input/index.tsx
+++ src/components/UI/Input/index.tsx
@@ -6,14 +6,14 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 }
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ error, ...rest }, ref) => {
+  ({ error, label, ...rest }, ref) => {
     return (
       <div className="grid pt-2 relative w-full">
         <label
           htmlFor={rest.name}
           className="absolute left-3 top-2 text-main-brown text-sm"
         >
-          {rest.label}
+          {label}
         </label>
         <input
           ref={ref}
@@ -28,4 +28,33 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
 
 Input.displayName = 'Input'
 
-export default Input
+interface TextAreaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  error?: string
+  label: string
+}
+
+const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  ({ error, label, ...rest }, ref) => {
+    return (
+      <div className="grid pt-2 relative w-full">
+        <label
+          htmlFor={rest.name}
+          className="absolute left-3 top-2 text-main-brown text-sm"
+        >
+          {label}
+        </label>
+        <textarea
+          ref={ref}
+          {...rest}
+          className="peer w-full border-2 bg-gray-50 text-gray-900 focus:ring-1 focus:ring-main-brown px-3 pt-4 pb-2 rounded-md"
+        />
+        {error && <span>{error}</span>}
+      </div>
+    )
+  },
+)
+
+TextArea.displayName = 'TextArea'
+
+export { Input, TextArea }
