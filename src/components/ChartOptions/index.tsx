aec60db7235b9b2a61350597dcd1aac7b82ed6c1
gusmadev <arthur.cabj@hotmail.com>
2025-01-10 10:41:49 -0300

    refactor: creates a non subscriber view to charts

--- src/components/ChartOptions/index.tsx
+++ src/components/ChartOptions/index.tsx
@@ -2,22 +2,39 @@
 
 import { useEffect, useState } from 'react'
 import { Chart } from '../Chart'
-import { TABLE_POSITIONS } from '@/constants/gloabal'
 import { handleSelection, handleSelectRange } from './helpers'
 import ChartOptionsButton from './ChartOptionsButton'
 import { Range } from '../Chart/types'
 import ColorDescription from '../ColorDescription'
 import { useTranslation } from 'react-i18next'
+import { GetTablePositions } from '@/app/_actions'
+import Link from 'next/link'
+import LoadingSpinner from '../UI/LoadingSpinner'
 
 export default function ChartOptions() {
   const [selectedPosition, setSelectedPosition] = useState<string>('UTG')
   const [selectedAction, setSelectedAction] = useState<string>('RFI')
   const [range, setRange] = useState<Range>({} as Range)
+  const [tablePositions, setTablePositions] = useState<string[]>([])
+  const [loading, setLoading] = useState<boolean>(true)
   const { t } = useTranslation()
+
+  async function handleTablePositions() {
+    const positions = await GetTablePositions()
+    setTablePositions(positions)
+    setLoading(false)
+  }
 
   useEffect(() => {
     setRange(handleSelectRange(selectedPosition, selectedAction))
   }, [selectedPosition, selectedAction])
+
+  useEffect(() => {
+    setLoading(true)
+    handleTablePositions()
+  }, [])
+
+  if (loading) return <LoadingSpinner />
 
   return (
     <section>
@@ -26,7 +43,7 @@ export default function ChartOptions() {
           {t('chart-options.select-position')}
         </h1>
         <section className="flex justify-between mb-4 max-sm:grid grid-cols-3 gap-1 max-sm:gap-x-10 max-sm:w-fit">
-          {TABLE_POSITIONS.map((position) => (
+          {tablePositions.map((position) => (
             <ChartOptionsButton
               key={position}
               position={position}
@@ -35,6 +52,16 @@ export default function ChartOptions() {
               handleBB={setSelectedAction}
             />
           ))}
+          {tablePositions.length === 1 && (
+            <i className="mt-1">
+              <Link href="/billing" className="underline-offset-1">
+                <span className="underline text-blue-300">
+                  {t('no-subscriber.subscribe')}
+                </span>
+              </Link>{' '}
+              {t('no-subscriber.description')}
+            </i>
+          )}
         </section>
         <h1 className="mb-2 text-main-gold w-fit">
           {t('chart-options.select-action')}
