f54eee4ffdb4b3a779ea12ee1d6a76b2b924b5aa
gusmadev <arthur.cabj@hotmail.com>
2025-01-14 11:40:21 -0300

    fix: aside layout on mobile

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -14,7 +14,7 @@ export default function Aside() {
   const photoSrc = user?.image || defaultLogo
 
   return (
-    <aside className="max-md:flex max-md:flex-row-reverse max-md:justify-end max-md:items-center max-md:mb-4">
+    <aside className="max-md:flex max-md:items-center max-md:mb-4 max-sm:-mt-4">
       <Image
         src={photoSrc}
         alt="Profile Image"
@@ -22,7 +22,7 @@ export default function Aside() {
         height={60}
         className="rounded-full w-10 h-10 sm:w-20 sm:h-20 md:w-20 md:h-20"
       />
-      <ul className="md:mt-8 mt-2 max-md:mr-4 max-md:space-x-4 max-md:flex justify-end ml-auto">
+      <ul className="md:mt-8 mt-2 max-md:space-x-4 max-md:flex ml-2 ">
         <li className="mb-2 cursor-pointer hover:text-main-gold">
           <Link href="/billing">{t('aside.subscription')}</Link>
         </li>
