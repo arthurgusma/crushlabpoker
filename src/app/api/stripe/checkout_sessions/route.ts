fcd012ef728b3c7f2d41f717a31ffbb1eb9d3a5e
gusmadev <arthur.cabj@hotmail.com>
2025-01-15 17:44:32 -0300

    fix: adjust new url path

--- src/app/api/stripe/checkout_sessions/route.ts
+++ src/app/api/stripe/checkout_sessions/route.ts
@@ -25,7 +25,7 @@ export async function POST(request: Request) {
       ],
       locale: language,
       mode: type,
-      return_url: `${origin}/pt-BR/billing/confirm?session_id={CHECKOUT_SESSION_ID}`,
+      return_url: `${origin}/en-US/billing/confirm?session_id={CHECKOUT_SESSION_ID}`,
     })
     return NextResponse.json({ clientSecret: session.client_secret })
   } catch (error) {
